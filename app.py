d07f4fc6e3aadcf79b23c54781f6f05ca7351a15
Alisevichandrew <dopysk@yandex.ru>
2022-01-17 22:19:48 +0300

    first wokking by locall host

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect, session
+
+app = Flask(__name__)
+
+@app.route('/')  
+def base():
+    return render_template('base.html')
+
+@app.route('/home/') 
+def home():
+    return render_template('home.html')
+
+@app.route('/dashboard/') 
+def dashboard():
+    return render_template('dashboard.html')
+
+app.run(port=5000)
 
