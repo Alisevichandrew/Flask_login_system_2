03b91a05b7457d3a6164fd6f55c0fd38935a55b1
Alisevichandrew <dopysk@yandex.ru>
2022-01-18 16:08:28 +0300

    create an account in home.html

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template, request, url_for, redirect, session
+from flask import Flask, render_template, request, url_for, redirect, session, jsonify
+from user.models import User
 
 app = Flask(__name__)
+# Create the user object
+class User:
 
+  def signup(self):
+
+    user = {
+      "_id": "",
+      "name": "",
+      "email": "",
+      "password": ""
+    }
+
+    return jsonify(user), 200
+
+# Routes
 @app.route('/')  
 def base():
     return render_template('base.html')
@@ -14,5 +29,13 @@ def home():
 def dashboard():
     return render_template('dashboard.html')
 
+@app.route('/user/signup/', methods=['GET'])
+def signup():
+    return User().signup()
+
 app.run(port=5000)
+
+if __name__ == "__main__":
+  app.run(debug=True)
+
 
