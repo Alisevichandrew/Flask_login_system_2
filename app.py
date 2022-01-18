e1b8e81222e703e3b23d73b3fb5508dfc4ca55ec
Alisevichandrew <dopysk@yandex.ru>
2022-01-18 20:26:03 +0300

    data of user

--- app.py
+++ app.py
@@ -1,20 +1,40 @@
 from flask import Flask, render_template, request, url_for, redirect, session, jsonify
+import uuid
+import pymongo
+from passlib.hash import pbkdf2_sha256
 from user.models import User
+# from app import db
 
 app = Flask(__name__)
+
+#Data base e.g "MongoDB Compass"
+client = pymongo.MongoClient('localhost', 27017)
+db = client.user_login_system #this name we will see in e.g. "MongoDB Compass"
+
 # Create the user object
 class User:
 
   def signup(self):
+    print(request.form) #open 'User data' in terminal by VS code
 
+# Personal data of user
     user = {
-      "_id": "",
-      "name": "",
-      "email": "",
-      "password": ""
+      "_id": uuid.uuid4().hex,
+      "name": request.form.get('name'),
+      "email": request.form.get('email'),
+      "password": request.form.get('password')
     }
+    # Encrypt the password
+    user['password'] = pbkdf2_sha256.encrypt(user['password'])
 
-    return jsonify(user), 200
+    # Chesk for existing email address
+    if db.users.find_one({ "email": user['email'] }):
+        return jsonify({"error": "Email address already in use"}), 400
+
+    if db.users.insert_one(user): 
+        return jsonify(user), 200
+
+    return jsonify({ "error": "Signup failed" }), 400
 
 # Routes
 @app.route('/')  
@@ -29,7 +49,7 @@ def home():
 def dashboard():
     return render_template('dashboard.html')
 
-@app.route('/user/signup/', methods=['GET'])
+@app.route('/user/signup/', methods=['POST'])
 def signup():
     return User().signup()
 
